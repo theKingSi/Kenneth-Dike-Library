47558331715ce782d2eff23a41184d5f7c171e69
theKingSi <ndunewesolomon.gmail.com>
2025-06-30 00:58:56 -0700

    New push

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,6 @@ import { Menu, X } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import Image from "next/image";
-
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
